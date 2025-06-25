c698da9f15ce47ed4071c558aa0410d186dd4fba
Sahithi-Bommineni <114471034+Sahithi-Bommineni@users.noreply.github.com>
2025-06-25 11:06:18 -0500

    Say  "Hello, World!" With Python

--- problem1.py
+++ problem1.py
@@ -1,4 +1,3 @@
-# Say  "Hello, World!" With Python
 # Here is a sample line of code that can be executed in Python:
 # print("Hello, World!")
 # You can just as easily store a string as a variable and then print it to stdout:
