c5b40995cb39765a48d110af9709bdf140dfc499
Sahithi-Bommineni <114471034+Sahithi-Bommineni@users.noreply.github.com>
2025-06-27 13:03:53 -0500

    NestedLists.py

--- NestedLists.py
+++ NestedLists.py
@@ -34,7 +34,19 @@
 # python students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
 # The lowest grade of 37.2 belongs to Tina. The second lowest grade of 37.21 belongs to both Harry and Berry, so we order their names alphabetically and print each name on a new line.
 
-
+if __name__ == '__main__':
+    students=[]
+    scores=[]
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        scores.append(score)
+        students.append([name,score])
+    scores = sorted(set([score for name, score in students]))
+    second_lowest = scores[1]
+    names=[name for name,score in students if score == second_lowest]
+    for name in sorted(names):
+        print(name)
 
 
 
