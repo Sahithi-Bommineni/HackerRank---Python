de4bfe0cd18e500a3c0fc9aa480144fd4a4584e2
Sahithi-Bommineni <114471034+Sahithi-Bommineni@users.noreply.github.com>
2025-07-07 13:12:35 -0500

    Lists.py

--- Lists.py
+++ Lists.py
@@ -45,3 +45,25 @@
 # [1, 5, 9, 10]
 # [9, 5, 1]
 
+if __name__ == '__main__':
+    N = int(input())
+    list=[]
+    for i in range(N):
+        command = input().split()
+        match(command[0]):
+            case 'insert':
+                list.insert(int(command[1]),int(command[2]))
+            case 'print':
+                print(list)
+            case 'remove':
+                list.remove(int(command[1]))
+            case 'sort':
+                list.sort()
+            case 'pop':
+                list.pop()
+            case 'append':
+                list.append(int(command[1]))
+            case 'reverse':
+                list.reverse()
+        
+
